4a15760265416de0a9df427d7ed35c065dec6718
JorgeCasas01 <jorgeluiscazas@gmail.com>
2024-06-21 03:06:08 -0600

    reportes Ciudadano

--- app/src/main/java/com/proyectomovil/snorb/ImageData.kt
+++ app/src/main/java/com/proyectomovil/snorb/ImageData.kt
@@ -4,5 +4,6 @@ data class ImageData(
     val imageUrl: String = "",
     val description: String = "",
     val date: String = "",
-    val location: String = ""
+    val location: String = "",
+    val userName: String = ""
 )
